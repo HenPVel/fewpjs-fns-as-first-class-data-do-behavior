dff5a274ddb0a95e67a18323057a85947feda97b
Henry <hvelas93@gmail.com>
2021-04-06 21:36:28 -0400

    got the functions to work but final test for display message doesn't pass

--- index.js
+++ index.js
@@ -9,5 +9,26 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet (value) {
+  let timeSplit = value.split(/[.:]/)
+  time = parseInt(timeSplit[0].concat(timeSplit[1]),10)
+  let x
+
+  if (time < 1200) {
+    x = "Good Morning"
+  } else if (time < 1700 && time >= 1200) {
+    x = "Good Afternoon"
+  } else {
+    x = "Good Evening"
+  }
+  console.log(x)
+  return x
+}
+
+function displayMessage (string) {
+  greeterContainer = document.querySelector('h1#greeting')
+  greeterContainer.innerText = string
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
